6f7071fe23eac4b53c1b193238060158524cc2d7
Jelena <jelena.agiejewa@gmail.com>
2024-02-05 11:59:31 +0100

    changed icon and contact__form

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 const btnChevron = document.querySelector('.contact__form--chevron');
 const select = document.querySelector('.contact__form-select');
+const inputChervon = document.querySelector('.contact__form-input--chevron');
 
 btnChevron.addEventListener('click', () => {
 	select.classList.toggle('show');
+	// inputChervon.classList.toggle('show');
+	btnChevron.classList.toggle('icon-rotate');
 });
